02c04ea479012024772d015872d5fa8af959ee15
Djaloud Mohamed <mrdjaloud@gmail.com>
2021-06-04 17:25:28 +0100

    mise en place du template et liaison des pages

---
+++ Professeurs/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Professeurs.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Professeurs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Trombinoscope()
+        {
+            return View();
+        }
+
+        public IActionResult email()
+        {
+            return View();
+        }
+
+        public IActionResult uploadFichier()
+        {
+            return View();
+        }
+
+        public IActionResult rechercheEtudiant()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
